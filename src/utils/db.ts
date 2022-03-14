67997d23fecc3b7ba276db1354dd833640d0d65c
Gustavo Maronato <gustavo@maronato.dev>
2022-03-14 00:12:23 -0300

    do not trace getPrisma

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,18 +1,13 @@
 import { PrismaClient } from "@prisma/client"
 
 import { createTelemetryMiddleware } from "./telemetry/db"
-import { startActiveSpan } from "./telemetry/trace"
 
 let client: PrismaClient | undefined = undefined
 
 export function getPrisma() {
-  return startActiveSpan("getPrisma", (span) => {
-    span.setAttribute("initialized", true)
-    if (!client) {
-      span.setAttribute("initialized", false)
-      client = new PrismaClient()
-      client.$use(createTelemetryMiddleware())
-    }
-    return client
-  })
+  if (!client) {
+    client = new PrismaClient()
+    client.$use(createTelemetryMiddleware())
+  }
+  return client
 }
