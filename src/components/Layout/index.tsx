001c2df1574d9c3451cb1c4d35aee6d808f5b6aa
Gustavo Maronato <maronato@uber.com>
2022-03-27 09:33:14 -0300

    remove footer from index and inset it in its own folder

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,12 @@
 import { FunctionComponent } from "react"
 
-import LogoTitle from "./LogoTitle"
-import ProfileButton from "./ProfileButton"
+import LogoTitle from "../LogoTitle"
+import ProfileButton from "../ProfileButton"
 
 const Layout: FunctionComponent = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="py-5 px-10 grid grid-cols-2 md:grid-cols-2 items-center">
+      <header className="py-5 px-10 grid grid-cols-2 md:grid-cols-2 items-center mx-auto w-full max-w-screen-xl">
         <div className="flex flex-row justify-start">
           <LogoTitle hide />
         </div>
@@ -17,9 +17,6 @@ const Layout: FunctionComponent = ({ children }) => {
       <main className="flex-grow px-5 w-full max-w-md md:max-w-screen-lg mx-auto my-10">
         {children}
       </main>
-      <footer className="py-20 px-10 bg-background-light dark:bg-background-dark text-slate-700 dark:text-slate-300 border-t border-slate-300 dark:border-gray-700">
-        hey
-      </footer>
     </div>
   )
 }
