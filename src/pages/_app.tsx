543443112d933b745bd1ba4063aaaf74b5b441c0
Gustavo Maronato <gustavo@maronato.dev>
2022-03-29 13:09:12 -0300

    added favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
+import Head from "next/head"
+
 import type { AppProps } from "next/app"
 
 import "@/styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Entre com Unicamp</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
