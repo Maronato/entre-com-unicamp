5991e2fd2f1c8e592ae30e50795fed25515778f2
Gustavo Maronato <gustavo@maronato.dev>
2022-03-31 20:33:08 -0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-sudo docker stack deploy -c <(docker-compose config) --with-registry-auth ecu
+VERSION=$(cat version | awk 'BEGIN{FS=OFS="."} {$2+=1} 1')
+
+#sudo VERSION=$VERSION docker-compose pull
+sudo VERSION=$VERSION bash -c 'VERSION=$VERSION docker stack deploy -c <(VERSION=$VERSION docker-compose config) --with-registry-auth ecu'
+
+echo "$VERSION" > version
+
